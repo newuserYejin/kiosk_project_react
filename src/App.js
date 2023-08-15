d12795fde221dd08a8c6c0d2d7184f8ed3bc81b3
jin <jin200126@gmail.com>
2023-08-15 17:17:23 +0900

    2023_8_15 은영이꺼 수정

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import SelectEat from '../src/selectEat/selecteat'
 import Routing from './Routing';
 import Index from './index';
 import BicOrder from '../src/BicOrder/BicOrder';
+import Paymethod from '../src/paymethod/paymethod'
+import OrderNum from  '../src/ordernum/ordernum'
+import Pay_Card from  './pay_card/pay_card'
+import Pay_Coupon from  './pay_coupon/pay_coupon'
 
 
 function App() {
   return (
     <>
-      <SelectEat/>
+      <Pay_Coupon/>
     </>
   );
 }

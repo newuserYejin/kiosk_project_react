118721a1dba320f2d909a4ea459e14c4aa8da589
jin <jin200126@gmail.com>
2023-08-15 17:29:14 +0900

    2023_8_15 4차

--- src/ordernum/ordernum.js
+++ src/ordernum/ordernum.js
@@ -20,12 +20,12 @@ function App() {
     return (
         <div className={css.all}>
             <div className={css.help}>
-                <img className={css.helpimg} src={helpImage} width="7.5%" height="7.5%" onclick={openHelp()}></img>
+                <img className={css.helpimg} src={helpImage} width="7.5%" height="7.5%" onClick={openHelp}></img>
             </div>
             <div className={css.ordernum}>
                 <div className={css.order}>
                     <div className={css.closeimg}>
-                        <img className={css.close} src={closeImage} width="6%" height="3%" onclick={Close()}></img>
+                        <img className={css.close} src={closeImage} width="6%" height="3%" onClick={Close}></img>
                     </div>
                     <div className={css.info}>
                         <p className={css.completed}>주문 완료</p>
@@ -34,7 +34,7 @@ function App() {
                     </div>
                 </div>
                 <div className={css.gohome_}>
-                    <button type="button" className={css.gohome} onclick={openHome()}>처음으로 돌아가기</button>
+                    <button type="button" className={css.gohome} onClick={openHome}>처음으로 돌아가기</button>
                 </div>
             </div>
         </div>
